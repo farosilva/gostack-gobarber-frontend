93bab8c59196a4565963d10ee78d92c729c68f89
Fabricio Silva <fabricio.roddrigo@gmail.com>
2020-07-31 23:48:32 -0300

    Using Unform

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form
+          initialData={{ email: 'fabricio.roddrigo@gmail.com' }}
+          onSubmit={handleSubmit}
+        >
+          <h1>Faça seu cadastro</h1>
+
+          <Input icon={FiUser} name="name" type="text" placeholder="Nome" />
+
+          <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="logon">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignUp;
